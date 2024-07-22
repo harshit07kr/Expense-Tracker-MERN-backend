498abf3fc9fbd8db124dfc626c83be8c11b917a7
HARSHIT <harshitkr9435@gmail.com>
2024-07-22 20:43:35 +0530

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,27 +3,23 @@ const User = require("../models/usermodel");
 
 async function requireAuth(req, res, next) {
   try {
-    // Read token from the Authorization header
-    const authHeader = req.headers['authorization'];
-    if (!authHeader) return res.sendStatus(401); // Unauthorized if no header
+    // Read token off cookies
+    const token = req.cookies.Authorization;
 
-    // Extract token and remove 'Bearer ' prefix
-    const token = authHeader.startsWith('Bearer ') ? authHeader.substring(7, authHeader.length) : authHeader;
-    
     // Decode the token
     const decoded = jwt.verify(token, process.env.SECRET);
 
     // Check expiration
-    if (Date.now() > decoded.exp * 1000) return res.sendStatus(401); // Convert exp to milliseconds
+    if (Date.now() > decoded.exp) return res.sendStatus(401);
 
     // Find user using decoded sub
     const user = await User.findById(decoded.sub);
     if (!user) return res.sendStatus(401);
 
-    // Attach user to req
+    // attach user to req
     req.user = user;
 
-    // Continue to next middleware
+    // continue on
     next();
   } catch (err) {
     return res.sendStatus(401);
