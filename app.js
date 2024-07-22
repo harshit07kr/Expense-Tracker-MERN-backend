e8ef332be43c333d7ed786cb1573b630ea0e1e51
HARSHIT <harshitkr9435@gmail.com>
2024-07-22 15:16:55 +0530

    cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 //middlewares
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:3000', // Your frontend URL
+  origin: 'https://sweet-monstera-672ffa.netlify.app', // Your frontend URL
   credentials: true,
 }));
   
